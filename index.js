8a1ba80adaa97a67293c0781097e881d1fb0e3c9
unknown <mmiq69@gmail.com>
2023-10-29 17:26:01 -0400

    export and import done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const { readFile, writeFile } = require('fs/promises');
 const inquirer = require('inquirer');
+const shapes = require('./lib/shapes');
 
-const generateSvg= ({color, shape, backgroundcolor })=>
-`const svgNew=[];
-const svgFile= document.createElementNS(svgNew, 'svg);
-svgFile.setAttribute("color", ${color});
-svgFile.setAttribute("shape", ${shape});
-svgFile.setAttribute("backgroundcolor", ${backgroundcolor});
-`
+const Circle = shapes.Circle;
+const Triangle=shapes.Triangle;
+const Square=shapes.Square;
+
+const generateSvg = ({ color, shape, backgroundcolor }) => {
+    let svgContent = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" style="background-color:${backgroundcolor}">`;
+
+    shape.forEach(selectedShape => {
+        let shapeSvg;
+        switch (selectedShape) {
+            case 'triangle':
+                shapeSvg = `<polygon points="50,0 100,100 0,100" fill="${color}" />`;
+                break;
+            case 'circle':
+                shapeSvg = `<circle cx="50" cy="50" r="50" fill="${color}" />`;
+                break;
+            case 'square':
+                shapeSvg = `<rect x="0" y="0" width="100" height="100" fill="${color}" />`;
+                break;
+            default:
+                shapeSvg = '';
+            }
+            svgContent += shapeSvg;
+        });
+    
+        svgContent += `</svg>`;
+        return svgContent;
+    };
 inquirer
 .prompt([
     {
@@ -17,7 +39,7 @@ inquirer
   
     },
     {
-        type: 'cehckbox',
+        type: 'checkbox',
         name: 'shape',
         message: 'What is the shape of your logo should look a like?',
         choices:['triangle', 'circle', 'square'],
@@ -28,5 +50,5 @@ inquirer
     message: 'What is your prefered color for shape?',},
 
 ]).then((answers)=>{
-
+    const svgContent = generateSvg(answers);
 })
